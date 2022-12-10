9e7e76cc44900aa26516a92f9b9a960bd1909148
mikhaelc09 <mikhael.chris09@gmail.com>
2022-12-10 14:40:18 +0700

    create janjiTemu seeder

---
+++ database/factories/JanjiTemuFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Dokter;
+use App\Models\Pasien;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\JanjiTemu>
+ */
+class JanjiTemuFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            "ps_id" => $this->faker->randomElement(Pasien::all()->pluck(['ps_id'])),
+            "dk_id" => $this->faker->randomElement(Dokter::all()->pluck(['dk_id'])),
+            "jt_tanggal" => $this->faker->dateTimeBetween('now', '+2 weeks'),
+        ];
+    }
+}
