89ab44ed7cbb7ecb0e44b97a8d75bdc485f33d8c
mikhaelc09 <mikhael.chris09@gmail.com>
2022-12-10 16:04:45 +0700

    create query for dokter_home

--- app/Http/Controllers/DokterController.php
+++ app/Http/Controllers/DokterController.php
@@ -2,17 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Dokter;
+use App\Models\JanjiTemu;
+use App\Models\Konsultasi;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class DokterController extends Controller
 {
     public function indexHome(Request $req)
     {
-        return view('dokter.home');
+        $user = Dokter::where("dk_email", Session::get("active")["email"])->first();
+        $janji_temu = JanjiTemu::where("dk_id", $user->dk_id)->get();
+        $konsultasi = Konsultasi::where("dk_id", $user->dk_id)->get();
+
+        return view('dokter.home', compact('janji_temu', 'konsultasi'));
     }
 
     public function indexJanji(Request $req)
     {
+        $user = Dokter::where("dk_email", Session::get("email"))->first();
+        
         return view('dokter.janjitemu');
     }
 
--- resources/views/dokter/home.blade.php
+++ resources/views/dokter/home.blade.php
@@ -5,5 +5,44 @@
 @endsection
 
 @section('main')
-<p>Home Dokter</p>
+Janji Temu:
+@foreach ($janji_temu as $a)
+    <table class="table">
+        <tr>
+            <th>je_id</th>
+            <th>ps_id</th>
+            <th>dk_id</th>
+            <th>jt_tanggal</th>
+            <th>created_at</th>
+            <th>updated_at</th>
+        </tr>
+        <tr>
+            <td>{{ $a->je_id }}</td>
+            <td>{{ $a->ps_id }}</td>
+            <td>{{ $a->dk_id }}</td>
+            <td>{{ $a->jt_tanggal }}</td>
+            <td>{{ $a->created_at }}</td>
+            <td>{{ $a->updated_at }}</td>
+        </tr>
+    </table>
+@endforeach
+Konsultasi:
+@foreach ($konsultasi as $a)
+    <table class="table">
+        <tr>
+            <td>ks_id</td>
+            <td>dk_id</td>
+            <td>ps_id</td>
+            <td>created_at</td>
+            <td>updated_at</td>
+        </tr>
+        <tr>
+            <td> {{ $a->ks_id}} </td>
+            <td> {{ $a->dk_id}} </td>
+            <td> {{ $a->ps_id}} </td>
+            <td> {{ $a->created_at}} </td>
+            <td> {{ $a->updated_at}} </td>
+        </tr>
+    </table>
+@endforeach
 @endsection
